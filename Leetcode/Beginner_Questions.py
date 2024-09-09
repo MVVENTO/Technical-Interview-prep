82a69e3820311428ada0cec7b6dcf3fa2bb1bca7
Mel_v <Mvvento@gmail.com>
2024-09-09 11:58:47 -0400

    Create Beginner_Questions.py

---
+++ Leetcode/Beginner_Questions.py
@@ -0,0 +1,14 @@
+# Beginner Questions once have a look:
+
+# Two Sum
+# Roman to Integer
+# Palindrome Number
+# Maximum Subarray
+# Remove Element
+# Contains Duplicate
+# Add Two Numbers
+# Majority Element
+# Remove Duplicates from Sorted Array
+# Valid Anagram
+# Group Anagrams
+# Practice them in a row for better understanding and please Upvote for more questions.
