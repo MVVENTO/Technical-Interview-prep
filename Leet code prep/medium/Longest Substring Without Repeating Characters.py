49f00132e21dbcb5035f0e994300a7200b292c44
Mel_v <Mvvento@gmail.com>
2025-01-08 15:09:40 -0500

    Create Longest Substring Without Repeating Characters.py

---
+++ Leet code prep/medium/Longest Substring Without Repeating Characters.py
@@ -0,0 +1,13 @@
+def length_of_longest_substring(s):
+    char_set = set()
+    l = 0
+    max_length = 0
+
+    for r in range(len(s)):
+        while s[r] in char_set:
+            char_set.remove(s[l])
+            l += 1
+        char_set.add(s[r])
+        max_length = max(max_length, r - l + 1)
+
+    return max_length
